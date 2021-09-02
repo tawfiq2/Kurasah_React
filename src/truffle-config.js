4075b6d4862481fbff512c37742aedb5a6524b54
tawfiq2 <tawfiqaljohani@gmail.com>
2021-09-02 19:34:56 +0300

    deploy dapp

--- src/truffle-config.js
+++ src/truffle-config.js
@@ -1,15 +1,30 @@
 
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic ="position subway ketchup fence drum flavor neglect scene drum hundred sibling blush"
+
 module.exports = {
+
   networks: {
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
     },
-  },
-  compilers: {
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/5c28437fcbe9442b9df078d743250da5"),
+      network_id: 4,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+   },
+   compilers: {
     solc: {
        version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
     }
-  }
-};
+  },
+ };
+
